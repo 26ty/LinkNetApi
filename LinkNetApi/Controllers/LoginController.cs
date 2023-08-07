efccba29d9f4e052fd64134839ab330d86a5119c
26ty <ch1342213422@gmail.com>
2023-08-07 10:57:57 +0800

    fix: 登入流程錯誤碼處理

--- LinkNetApi/Controllers/LoginController.cs
+++ LinkNetApi/Controllers/LoginController.cs
@@ -24,17 +24,49 @@ namespace LinkNetApi.Controllers
         [HttpPost]
         public async Task<IActionResult> Login([FromBody] Login request)
         {
-            // 查找符合帳號的用戶
-            var user = await _context.User.FirstOrDefaultAsync(u => u.username == request.username);
+            //// 查找符合帳號的用戶
+            //var user = await _context.User.FirstOrDefaultAsync(u => u.username == request.username);
 
-            // 如果找不到用戶或密碼不正確，返回失敗提示
-            if (user == null || user.password != request.password)
+            //// 如果找不到用戶或密碼不正確，返回失敗提示
+            //if (user == null || user.password != request.password)
+            //{
+            //    //return BadRequest("帳號或密碼輸入錯誤");
+            //    return BadRequest(new { massage = "帳號或密碼輸入錯誤，登入失敗" });
+            //}
+
+            //// 驗證成功，返回成功提示
+            //return Ok(new {status=200, message= "登入成功" });
+            // 檢查是否輸入了帳號和密碼
+            if (string.IsNullOrEmpty(request.username) || string.IsNullOrEmpty(request.password))
             {
-                return BadRequest("帳號或密碼輸入錯誤");
+                return BadRequest(new { code = 400, message = "未輸入帳號或密碼！" });
             }
 
-            // 驗證成功，返回成功提示
-            return Ok("登入成功");
+            try
+            {
+                // 查找符合帳號的用戶
+                var user = await _context.User.FirstOrDefaultAsync(u => u.username == request.username);
+
+                // 如果找不到用戶，返回無此帳號提示
+                if (user == null)
+                {
+                    return StatusCode(422,new { code = 422, message = "無此帳號，是否要進行註冊？" });
+                }
+
+                // 檢查密碼是否正確
+                if (user.password != request.password)
+                {
+                    return Unauthorized(new { code = 401, message = "密碼錯誤!" });
+                }
+
+                // 驗證成功，返回成功提示
+                return Ok(new { code = 200, message = "登入成功！" });
+            }
+            catch (Exception)
+            {
+                // 發生異常，可能是伺服器無連線等問題
+                return StatusCode(500, new { code = 500, message = "伺服器無連線，請聯絡伺服器管理人員！" });
+            }
         }
     }
 }
